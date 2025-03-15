88ee46a1844fd769e3c501684a6aaaf0f7b71643
Will Hopkins <willothyh@gmail.com>
2025-03-15 15:40:22 -0700

    mcp server structure

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,25 @@
+import process from "node:process";
+
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 import { z } from "zod";
 
-// Create server instance
 const server = new McpServer({
-  name: "weather",
-  version: "1.0.0",
+  name: "homebridge",
+  version: "0.1.0",
+});
+
+// server.tool("name", "description", input_schema, async (input) => {})
+
+async function main() {
+  const transport = new StdioServerTransport();
+
+  await server.connect(transport);
+
+  console.error("Homebridge server is running...");
+}
+
+main().catch((err) => {
+  console.error("Error running server:", err);
+  process.exit(1);
 });
